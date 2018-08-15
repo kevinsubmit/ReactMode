627483699b89931bad066aa26171a1ed1b01ea51
kevin <barry.axure@gmail.com>
2018-08-15 21:37:12 +0800

    更新模板

--- src/util/request.js
+++ src/util/request.js
@@ -1,24 +1,26 @@
 import axios from 'axios';
 const request = axios.create({
-  baseURL: 'http://pc.l5uc.com',
+  baseURL: window.baseURL,
   // 以下解决跨域
   proxy: {
     host: '127.0.0.1',
     port: 3000
   },
 });
-
-request.interceptors.request.use(function (config) {
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
-
+request.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  });
 // Add a response interceptor
-request.interceptors.response.use(function (response) {
-  return response;
-}, function (error) {
-  return Promise.reject(error);
-});
+request.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  });
 
 export default request
